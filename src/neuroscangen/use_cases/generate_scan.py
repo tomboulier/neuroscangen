7b2750f303105993d188841e962a13a0f704c1df
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2025-01-22 18:55:33 +0100

    feat: add BrainScan class

--- src/neuroscangen/use_cases/generate_scan.py
+++ src/neuroscangen/use_cases/generate_scan.py
@@ -1,3 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class BrainScan:
+    def __init__(self):
+        pass
+
+
 class GenerateScan:
     def __call__(self):
-        return "A brain scan"
+        return BrainScan()
